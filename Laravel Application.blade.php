46268b727371310bf99bac703aec428cd2907667
apari <aparicio.enice1402@gmail.com>
2022-09-16 08:20:59 +0800

    First commit

---
+++ Laravel Application.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel_Application | Application name</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Laravel Application</h1>
+    <a href="{{url('/')}}">Home</a> | Application name 
+    <a href="{{url('/Developer')}}">  | Developer</a> 
+</body>
+</html>
---
+++ developer.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel_Application</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Denice Jane Aparicio</h1>
+    <h1>BSIT 3</h1>
+    <a href="{{url('/')}}">Home </a> 
+    <a href="{{url('/Application name')}}">| Application name  </a> | Developer  
+ 
+</body>
+</html>
