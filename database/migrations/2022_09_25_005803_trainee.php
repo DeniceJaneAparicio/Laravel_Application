f3849c0eea68e4784af6f4c80383adbe9b579089
apari <aparicio.enice1402@gmail.com>
2022-09-25 16:39:42 +0800

    First commit

---
+++ database/migrations/2022_09_25_005803_trainee.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainees', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->unsigned();
+            $table->string('course_id')->unsigned();
+            $table->string('status');
+            $table->timestamps();
+            //foreign key
+            $table->foreign('user_id')->references('username')->on('users');
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainees');
+    }
+};
